7d86f64ccc2c85e02208aecfba5e1883b3a8c311
Ishan Khare <ishankhare07@gmail.com>
2021-02-11 20:30:02 +0530

    add prototype pattern
    Signed-off-by: Ishan Khare <ishankhare07@gmail.com>

---
+++ internal/pkg/creational/prototype/cloner.go
@@ -0,0 +1,76 @@
+package prototype
+
+import (
+	"errors"
+	"fmt"
+)
+
+type ShirtCloner interface {
+	GetClone(s int) (ItemInfoGetter, error)
+}
+
+const (
+	White = iota
+	Black
+	Blue
+)
+
+func GetShirtsCloner() ShirtCloner {
+	return &ShirtsCache{}
+}
+
+var whitePrototype *Shirt = &Shirt{
+	Price: 15.00,
+	SKU:   "empty",
+	Color: White,
+}
+
+var blackPrototype *Shirt = &Shirt{
+	Price: 16.00,
+	SKU:   "empty",
+	Color: Black,
+}
+
+var bluePrototype *Shirt = &Shirt{
+	Price: 17.00,
+	SKU:   "empty",
+	Color: Blue,
+}
+
+type ShirtsCache struct{}
+
+func (this *ShirtsCache) GetClone(s int) (ItemInfoGetter, error) {
+	switch s {
+	case White:
+		newItem := *whitePrototype
+		return &newItem, nil
+	case Black:
+		newItem := *blackPrototype
+		return &newItem, nil
+	case Blue:
+		newItem := *bluePrototype
+		return &newItem, nil
+	default:
+		return nil, errors.New("Shirt model not recognized")
+	}
+}
+
+type ItemInfoGetter interface {
+	GetInfo() string
+}
+
+type ShirtColor byte
+
+type Shirt struct {
+	Price float32
+	SKU   string
+	Color ShirtColor
+}
+
+func (s *Shirt) GetInfo() string {
+	return fmt.Sprintf("Shirt with SKU '%s' and Color id %d that costs %f\n", s.SKU, s.Color, s.Price)
+}
+
+func (s *Shirt) GetPrice() float32 {
+	return s.Price
+}
